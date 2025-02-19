a5d471b8266a72c115f408ad03c99bd0bda7da2c
PoshPanorama <poshpanorama7@gmail.com>
2025-02-19 11:08:35 +0900

    completed

--- backend/db/connectToMongoDB.js
+++ backend/db/connectToMongoDB.js
@@ -1,12 +1,17 @@
 import mongoose from "mongoose";
 
 const connectToMongoDB = async () => {
-	try {
-		await mongoose.connect(process.env.MONGO_DB_URI);
-		console.log("Connected to MongoDB");
-	} catch (error) {
-		console.log("Error connecting to MongoDB", error.message);
-	}
+    try {
+        // Ensure process.env.MONGO_URI is being correctly accessed
+        if (!process.env.MONGO_URI) {
+            console.log("MONGO_URI is not defined in the environment variables.");
+        } else {
+            await mongoose.connect(process.env.MONGO_URI);
+            console.log("Connected to MongoDB");
+        }
+    } catch (error) {
+        console.log("Error connecting to MongoDB", error.message);
+    }
 };
 
 export default connectToMongoDB;
